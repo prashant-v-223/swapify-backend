5dd487d0057e516951a58e3efe13647015468fb8
anshu sharma <anshusharma6327@gmail.com>
2023-07-05 23:00:38 +0530

    reset password route added

--- src/router/Users.js
+++ src/router/Users.js
@@ -24,7 +24,9 @@ const transporter = nodemailer.createTransport({
 router.post("/", async (req, res) => {
   const { email, password, name } = req.body;
   if (!email || !password || !name) {
-    res.status(403).send("Please fill all the data");
+    res.status(403).json({
+      message: "Please fill all the fields",
+    });
   } else {
     console.log(email, password, name);
     try {
@@ -47,15 +49,15 @@ router.post("/", async (req, res) => {
       let userInfo = new Users(user);
       let IsEmail = await Users.findOne({ email: req.body.email });
       if (IsEmail) {
-        res.status(401).json({ result: "User Already Exists!" });
+        res.status(403).json({ result: "Account already exists" });
       } else {
         await userInfo.save();
         res.json({ result: "Otp has been sent successfully !" });
-        transporter.sendMail(mailData, (error, info) => {
-          if (error) {
-            res.status(500).send("Server error");
-          }
-        });
+        // transporter.sendMail(mailData, (error, info) => {
+        //   if (error) {
+        //     res.status(500).send("Server error");
+        //   }
+        // });
       }
     } catch (error) {
       console.log(error);
@@ -64,7 +66,7 @@ router.post("/", async (req, res) => {
   }
 });
 
-router.post("/verify", async (req, res) => {
+router.post("/verify-otp", async (req, res) => {
   try {
     let IsValid = await Users.findOne({
       $and: [{ email: req.body.email }, { otp: req.body.otp }],
@@ -123,103 +125,67 @@ router.post("/login", async (req, res) => {
   }
 });
 
-// Step 1: Request password reset
-router.post("/forgot-password", async (req, res) => {
+// Handle password update
+router.post("/reset-password", async (req, res) => {
+  const otp = Math.floor(Math.random() * 9000 + 1000);
+  const mailData = {
+    from: process.env.EMAIL,
+    to: req.body.email,
+    subject: "Verifcation code for password reset",
+    text: null,
+    html: `<span>Your Verification code is ${otp}</span>`,
+  };
   const { email } = req.body;
   if (!email) {
-    return res.status(403).send("Please provide an email address");
+    return res.status(403).json({
+      message: "Please fill all the fields",
+    });
   }
-
   try {
     const user = await Users.findOne({ email });
     if (!user) {
-      return res.status(401).send("User not found");
+      return res.status(403).json({
+        message: "User with this email does not exist",
+      });
+    } else {
+      user.otp = otp;
+      await user.save();
+      res.json({ message: "Otp has been sent successfully !" });
+      // transporter.sendMail(mailData, (error, info) => {
+      //   if (error) {
+      //     res.status(500).send("Server error");
+      //   }
+      // });
     }
-
-    // Generate a random token for password reset
-    const resetToken = crypto.randomBytes(20).toString("hex");
-    user.resetToken = resetToken;
-    user.resetTokenExpiration = Date.now() + 3600000; // Token expires in 1 hour
-    await user.save();
-
-    const resetLink = `${process.env.SERVER_URL}/reset-password?token=${resetToken}`;
-    console.log(resetLink);
-    const mailData = {
-      from: process.env.EMAIL,
-      to: email,
-      subject: "Password Reset",
-      text: `You are receiving this email because you have requested to reset your password. Please click on the following link to reset your password:\n\n${resetLink}`,
-    };
-    transporter.sendMail(mailData, (error, info) => {
-      if (error) {
-        console.log(error);
-        res.status(500).send("Server error");
-      } else {
-        res.status(200).send("Password reset link sent to your email");
-      }
-    });
   } catch (error) {
     console.log(error);
     res.status(500).send("Server error");
   }
 });
 
-// Step 2: Handle password reset link
-router.get("/reset-password", async (req, res) => {
-  const { token } = req.query;
-  console.log(req.query);
-  if (!token) {
-    console.log("No token", token);
-    return res.status(400).send("Invalid token");
-  }
-
+router.put("/verify-reset-otp", async (req, res) => {
   try {
-    const user = await Users.findOne({
-      resetToken: token,
-      resetTokenExpiration: { $gt: Date.now() },
+    let salt = await bcrypt.genSalt(saltround);
+    let hash_password = await bcrypt.hash(req.body.password, salt);
+    let IsValid = await Users.findOne({
+      $and: [{ email: req.body.email }, { otp: req.body.otp }],
     });
-    if (!user) {
-      return res.status(400).send("Invalid or expired reset token");
+    if (IsValid) {
+      await Users.findOneAndUpdate(
+        { email: req.body.email },
+        { password: hash_password },
+        {
+          returnOriginal: false,
+        }
+      );
+      res.status(200).json({ message: "You password have been changed successfully !" });
+    } else {
+      res.status(401).json({ message: "Wrong Otp !" });
     }
-    res.status(200).json({
-      message: "Password reset link is valid",
-    });
   } catch (error) {
     console.log(error);
-    res.status(500).send("Server error");
+    res.status(500).json({ message: "Server error" });
   }
 });
-
-// Step 3: Handle password update
-router.put("/reset-password", async (req, res) => {
-  const { token, password } = req.body;
-  if (!token || !password) {
-    return res.status(400).send("Invalid request");
-  }
-
-  try {
-    const user = await Users.findOne({
-      resetToken: token,
-      resetTokenExpiration: { $gt: Date.now() },
-    });
-    if (!user) {
-      return res.status(400).send("Invalid or expired reset token");
-    }
-    // Hash the new password
-    const salt = await bcrypt.genSalt(saltround);
-    const hashPassword = await bcrypt.hash(password, salt);
-    user.password = hashPassword;
-    user.resetToken = undefined;
-    user.resetTokenExpiration = undefined;
-    await user.save();
-    res.status(200).json({
-      message: "Password updated successfully",
-    });
-  } catch (error) {
-    console.log(error);
-    res.status(500).send("Server error");
-  }
-});
-
 
 module.exports = router;
