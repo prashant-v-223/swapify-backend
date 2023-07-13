bc8bee52a3e8f72b11072c60059fe117962668e5
prashant-v-223 <prashantvadhvana@gmail.com>
2023-07-13 16:57:04 +0530

    asd

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ const app = express();
 app.use(express.json());
 app.use(cors({ origin: "*", credentials: true }));
 app.use("/users", UserRoute);
-
+app.get("/", async (req, res) => {
+  res.send("server is run")
+})
 app.listen(process.env.PORT, () => {
   console.log(`Server is running at port ${process.env.PORT}`);
 });
